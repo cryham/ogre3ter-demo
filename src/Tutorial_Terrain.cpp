6546d1af0576f91c476f1e6f0a3910cf19ab911f
cryham <cryham@gmail.com>
2022-02-19 13:46:50 +0100

    Original sources from 26 Jan 2022 with Tutorial_Terrain, OgreCommon and CMake. Ogre 2.3

---
+++ src/Tutorial_Terrain.cpp
@@ -0,0 +1,262 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2021 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+
+#include "GraphicsSystem.h"
+#include "Tutorial_TerrainGameState.h"
+
+#include "OgreRoot.h"
+#include "Compositor/OgreCompositorManager2.h"
+#include "OgreConfigFile.h"
+#include "OgreWindow.h"
+
+#include "Compositor/OgreCompositorWorkspace.h"
+#include "OgreArchiveManager.h"
+#include "OgreHlmsManager.h"
+#include "Terra/Hlms/OgreHlmsTerra.h"
+#include "Terra/TerraWorkspaceListener.h"
+
+//Declares WinMain / main
+#include "MainEntryPointHelper.h"
+#include "System/Android/AndroidSystems.h"
+#include "System/MainEntryPoints.h"
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE || OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS
+    #include "OSX/macUtils.h"
+    #if OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS
+        #include "System/iOS/iOSUtils.h"
+    #else
+        #include "System/OSX/OSXUtils.h"
+    #endif
+#endif
+
+namespace Demo
+{
+    class Tutorial_TerrainGraphicsSystem : public GraphicsSystem
+    {
+        Ogre::TerraWorkspaceListener *mTerraWorkspaceListener;
+
+        virtual void stopCompositor( void )
+        {
+            if( mWorkspace )
+                mWorkspace->removeListener( mTerraWorkspaceListener );
+            delete mTerraWorkspaceListener;
+            mTerraWorkspaceListener = 0;
+        }
+
+        virtual Ogre::CompositorWorkspace *setupCompositor()
+        {
+            using namespace Ogre;
+            CompositorManager2 *compositorManager = mRoot->getCompositorManager2();
+
+            CompositorWorkspace *workspace = compositorManager->addWorkspace(
+                mSceneManager, mRenderWindow->getTexture(), mCamera, "Tutorial_TerrainWorkspace", true );
+
+            if( !mTerraWorkspaceListener )
+            {
+                HlmsManager *hlmsManager = mRoot->getHlmsManager();
+                Hlms *hlms = hlmsManager->getHlms( HLMS_USER3 );
+                OGRE_ASSERT_HIGH( dynamic_cast<HlmsTerra *>( hlms ) );
+                mTerraWorkspaceListener = new TerraWorkspaceListener( static_cast<HlmsTerra *>( hlms ) );
+            }
+            workspace->addListener( mTerraWorkspaceListener );
+
+            return workspace;
+        }
+
+        virtual void setupResources(void)
+        {
+            GraphicsSystem::setupResources();
+
+            Ogre::ConfigFile cf;
+            cf.load( AndroidSystems::openFile( mResourcePath + "resources2.cfg" ) );
+
+            Ogre::String originalDataFolder = cf.getSetting( "DoNotUseAsResource", "Hlms", "" );
+
+            if( originalDataFolder.empty() )
+                originalDataFolder = AndroidSystems::isAndroid() ? "/" : "./";
+            else if( *(originalDataFolder.end() - 1) != '/' )
+                originalDataFolder += "/";
+
+            const char *c_locations[5] =
+            {
+                "2.0/scripts/materials/Tutorial_Terrain",
+                "2.0/scripts/materials/Tutorial_Terrain/GLSL",
+                "2.0/scripts/materials/Tutorial_Terrain/HLSL",
+                "2.0/scripts/materials/Tutorial_Terrain/Metal",
+                "2.0/scripts/materials/Postprocessing/SceneAssets"
+            };
+
+            for( size_t i=0; i<5; ++i )
+            {
+                Ogre::String dataFolder = originalDataFolder + c_locations[i];
+                addResourceLocation( dataFolder, getMediaReadArchiveType(), "General" );
+            }
+        }
+
+        virtual void registerHlms(void)
+        {
+            GraphicsSystem::registerHlms();
+
+            Ogre::ConfigFile cf;
+            cf.load( AndroidSystems::openFile( mResourcePath + "resources2.cfg" ) );
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE || OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS
+            Ogre::String rootHlmsFolder = Ogre::macBundlePath() + '/' +
+                                          cf.getSetting( "DoNotUseAsResource", "Hlms", "" );
+#else
+            Ogre::String rootHlmsFolder = mResourcePath +
+                                          cf.getSetting( "DoNotUseAsResource", "Hlms", "" );
+#endif
+            if( rootHlmsFolder.empty() )
+                rootHlmsFolder = AndroidSystems::isAndroid() ? "/" : "./";
+            else if( *(rootHlmsFolder.end() - 1) != '/' )
+                rootHlmsFolder += "/";
+
+            Ogre::RenderSystem *renderSystem = mRoot->getRenderSystem();
+
+            Ogre::String shaderSyntax = "GLSL";
+            if (renderSystem->getName() == "OpenGL ES 2.x Rendering Subsystem")
+                shaderSyntax = "GLSLES";
+            if( renderSystem->getName() == "Direct3D11 Rendering Subsystem" )
+                shaderSyntax = "HLSL";
+            else if( renderSystem->getName() == "Metal Rendering Subsystem" )
+                shaderSyntax = "Metal";
+
+            Ogre::String mainFolderPath;
+            Ogre::StringVector libraryFoldersPaths;
+            Ogre::StringVector::const_iterator libraryFolderPathIt;
+            Ogre::StringVector::const_iterator libraryFolderPathEn;
+
+            Ogre::ArchiveManager &archiveManager = Ogre::ArchiveManager::getSingleton();
+
+            Ogre::HlmsTerra *hlmsTerra = 0;
+            Ogre::HlmsManager *hlmsManager = mRoot->getHlmsManager();
+
+            {
+                //Create & Register HlmsTerra
+                //Get the path to all the subdirectories used by HlmsTerra
+                Ogre::HlmsTerra::getDefaultPaths( mainFolderPath, libraryFoldersPaths );
+                Ogre::Archive *archiveTerra = archiveManager.load( rootHlmsFolder + mainFolderPath,
+                                                                   getMediaReadArchiveType(), true );
+                Ogre::ArchiveVec archiveTerraLibraryFolders;
+                libraryFolderPathIt = libraryFoldersPaths.begin();
+                libraryFolderPathEn = libraryFoldersPaths.end();
+                while( libraryFolderPathIt != libraryFolderPathEn )
+                {
+                    Ogre::Archive *archiveLibrary = archiveManager.load(
+                        rootHlmsFolder + *libraryFolderPathIt, getMediaReadArchiveType(), true );
+                    archiveTerraLibraryFolders.push_back( archiveLibrary );
+                    ++libraryFolderPathIt;
+                }
+
+                //Create and register the terra Hlms
+                hlmsTerra = OGRE_NEW Ogre::HlmsTerra( archiveTerra, &archiveTerraLibraryFolders );
+                hlmsManager->registerHlms( hlmsTerra );
+            }
+
+            //Add Terra's piece files that customize the PBS implementation.
+            //These pieces are coded so that they will be activated when
+            //we set the HlmsPbsTerraShadows listener and there's an active Terra
+            //(see Tutorial_TerrainGameState::createScene01)
+            Ogre::Hlms *hlmsPbs = hlmsManager->getHlms( Ogre::HLMS_PBS );
+            Ogre::Archive *archivePbs = hlmsPbs->getDataFolder();
+            Ogre::ArchiveVec libraryPbs = hlmsPbs->getPiecesLibraryAsArchiveVec();
+            libraryPbs.push_back( Ogre::ArchiveManager::getSingletonPtr()->load(
+                                      rootHlmsFolder + "Hlms/Terra/" + shaderSyntax + "/PbsTerraShadows",
+                                      getMediaReadArchiveType(), true ) );
+            hlmsPbs->reloadFrom( archivePbs, &libraryPbs );
+        }
+
+    public:
+        Tutorial_TerrainGraphicsSystem( GameState *gameState ) :
+            GraphicsSystem( gameState ),
+            mTerraWorkspaceListener( 0 )
+        {
+        }
+    };
+
+    void MainEntryPoints::createSystems( GameState **outGraphicsGameState,
+                                         GraphicsSystem **outGraphicsSystem,
+                                         GameState **outLogicGameState,
+                                         LogicSystem **outLogicSystem )
+    {
+        Tutorial_TerrainGameState *gfxGameState = new Tutorial_TerrainGameState(
+        "--- !!! NOTE: THIS SAMPLE IS A WORK IN PROGRESS !!! ---\n"
+        "This is an advanced tutorial that shows several things working together:\n"
+        "   * Own Hlms implementation to render the terrain\n"
+        "   * Vertex buffer-less rendering: The terrain is generated purely using SV_VertexID "
+        "tricks and a heightmap texture\n"
+        "   * Hlms customizations to PBS to make terrain shadows affect regular objects\n"
+        "   * Compute Shaders to generate terrain shadows every frame\n"
+        "   * Common terrain functionality such as loading the heightmap, generating normals, LOD.\n"
+        "The Terrain system is called 'Terra' and has been isolated under the Terra folder like\n"
+        "a component for easier copy-pasting into your own projects.\n\n"
+        "Because every project has its own needs regarding terrain rendering, we're not providing\n"
+        "Terra as an official Component, but rather as a tutorial; where users can copy paste our\n"
+        "implementation and use it as is, or make their own tweaks or enhancements.\n\n"
+        "This sample depends on the media files:\n"
+        "   * Samples/Media/2.0/scripts/Compositors/Tutorial_Terrain.compositor\n"
+        "   * Samples/Media/2.0/materials/Tutorial_Terrain/*.*\n"
+        "   * Samples/Media/2.0/materials/Common/GLSL/GaussianBlurBase_cs.glsl\n"
+        "   * Samples/Media/2.0/materials/Common/HLSL/GaussianBlurBase_cs.hlsl\n"
+        "   * Samples/Media/Hlms/Terra/*.*\n" );
+
+        Tutorial_TerrainGraphicsSystem *graphicsSystem =
+                new Tutorial_TerrainGraphicsSystem( gfxGameState );
+
+        gfxGameState->_notifyGraphicsSystem( graphicsSystem );
+
+        *outGraphicsGameState = gfxGameState;
+        *outGraphicsSystem = graphicsSystem;
+    }
+
+    void MainEntryPoints::destroySystems( GameState *graphicsGameState,
+                                          GraphicsSystem *graphicsSystem,
+                                          GameState *logicGameState,
+                                          LogicSystem *logicSystem )
+    {
+        delete graphicsSystem;
+        delete graphicsGameState;
+    }
+
+    const char* MainEntryPoints::getWindowTitle(void)
+    {
+        return "Tutorial: Terrain";
+    }
+}
+
+#if OGRE_PLATFORM != OGRE_PLATFORM_ANDROID
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+INT WINAPI WinMainApp( HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR strCmdLine, INT nCmdShow )
+#else
+int mainApp( int argc, const char *argv[] )
+#endif
+{
+    return Demo::MainEntryPoints::mainAppSingleThreaded( DEMO_MAIN_ENTRY_PARAMS );
+}
+#endif
---
+++ src/Tutorial_TerrainGameState.cpp
@@ -0,0 +1,294 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2021 Torus Knot Software Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-----------------------------------------------------------------------------
+*/
+
+#include "Tutorial_TerrainGameState.h"
+#include "CameraController.h"
+#include "GraphicsSystem.h"
+#include "Utils/MeshUtils.h"
+
+#include "OgreSceneManager.h"
+
+#include "OgreRoot.h"
+#include "Vao/OgreVaoManager.h"
+#include "Vao/OgreVertexArrayObject.h"
+
+#include "OgreCamera.h"
+#include "OgreWindow.h"
+
+#include "Terra/Hlms/OgreHlmsTerra.h"
+#include "Terra/Hlms/PbsListener/OgreHlmsPbsTerraShadows.h"
+#include "Terra/Terra.h"
+#include "Terra/TerraShadowMapper.h"
+#include "OgreHlmsManager.h"
+#include "OgreHlms.h"
+#include "Compositor/OgreCompositorManager2.h"
+#include "Compositor/OgreCompositorWorkspace.h"
+
+#include "OgreTextureGpuManager.h"
+
+#include "OgreLwString.h"
+#include "OgreGpuProgramManager.h"
+
+#include "OgreItem.h"
+
+using namespace Demo;
+
+namespace Demo
+{
+    Tutorial_TerrainGameState::Tutorial_TerrainGameState( const Ogre::String &helpDescription ) :
+        TutorialGameState( helpDescription ),
+        mLockCameraToGround( false ),
+        mTimeOfDay( Ogre::Math::PI * /*0.25f*//*0.55f*/0.1f ),
+        mAzimuth( 0 ),
+        mTerra( 0 ),
+        mSunLight( 0 ),
+        mHlmsPbsTerraShadows( 0 )
+    {
+    }
+    //-----------------------------------------------------------------------------------
+    void Tutorial_TerrainGameState::createScene01(void)
+    {
+        Ogre::Root *root = mGraphicsSystem->getRoot();
+        Ogre::SceneManager *sceneManager = mGraphicsSystem->getSceneManager();
+
+        // Render terrain after most objects, to improve performance by taking advantage of early Z
+        mTerra = new Ogre::Terra( Ogre::Id::generateNewId<Ogre::MovableObject>(),
+                                  &sceneManager->_getEntityMemoryManager( Ogre::SCENE_STATIC ),
+                                  sceneManager, 11u, root->getCompositorManager2(),
+                                  mGraphicsSystem->getCamera(), false );
+        mTerra->setCastShadows( false );
+
+        //mTerra->load( "Heightmap.png", Ogre::Vector3::ZERO, Ogre::Vector3( 256.0f, 1.0f, 256.0f ), false );
+        //mTerra->load( "Heightmap.png", Ogre::Vector3( 64.0f, 0, 64.0f ), Ogre::Vector3( 128.0f, 5.0f, 128.0f ), false );
+        //mTerra->load( "Heightmap.png", Ogre::Vector3( 64.0f, 0, 64.0f ), Ogre::Vector3( 1024.0f, 5.0f, 1024.0f ), false );
+        //mTerra->load( "Heightmap.png", Ogre::Vector3( 64.0f, 0, 64.0f ), Ogre::Vector3( 4096.0f * 4, 15.0f * 64.0f*4, 4096.0f * 4 ), false );
+        mTerra->load( "Heightmap.png", Ogre::Vector3( 64.0f, 4096.0f * 0.5f, 64.0f ), Ogre::Vector3( 4096.0f, 4096.0f, 4096.0f ), false, false );
+        //mTerra->load( "Heightmap.png", Ogre::Vector3( 64.0f, 4096.0f * 0.5f, 64.0f ), Ogre::Vector3( 14096.0f, 14096.0f, 14096.0f ), false );
+
+        Ogre::SceneNode *rootNode = sceneManager->getRootSceneNode( Ogre::SCENE_STATIC );
+        Ogre::SceneNode *sceneNode = rootNode->createChildSceneNode( Ogre::SCENE_STATIC );
+        sceneNode->attachObject( mTerra );
+
+        Ogre::HlmsManager *hlmsManager = root->getHlmsManager();
+        Ogre::HlmsDatablock *datablock = hlmsManager->getDatablock( "TerraExampleMaterial" );
+//        Ogre::HlmsDatablock *datablock = hlmsManager->getHlms( Ogre::HLMS_USER3 )->getDefaultDatablock();
+//        Ogre::HlmsMacroblock macroblock;
+//        macroblock.mPolygonMode = Ogre::PM_WIREFRAME;
+        //datablock->setMacroblock( macroblock );
+        mTerra->setDatablock( datablock );
+
+        {
+            mHlmsPbsTerraShadows = new Ogre::HlmsPbsTerraShadows();
+            mHlmsPbsTerraShadows->setTerra( mTerra );
+            //Set the PBS listener so regular objects also receive terrain shadows
+            Ogre::Hlms *hlmsPbs = root->getHlmsManager()->getHlms( Ogre::HLMS_PBS );
+            hlmsPbs->setListener( mHlmsPbsTerraShadows );
+        }
+
+        mSunLight = sceneManager->createLight();
+        Ogre::SceneNode *lightNode = rootNode->createChildSceneNode();
+        lightNode->attachObject( mSunLight );
+        mSunLight->setPowerScale( Ogre::Math::PI );
+        mSunLight->setType( Ogre::Light::LT_DIRECTIONAL );
+        mSunLight->setDirection( Ogre::Vector3( -1, -1, -1 ).normalisedCopy() );
+
+        sceneManager->setAmbientLight( Ogre::ColourValue( 0.33f, 0.61f, 0.98f ) * 0.01f,
+                                       Ogre::ColourValue( 0.02f, 0.53f, 0.96f ) * 0.01f,
+                                       Ogre::Vector3::UNIT_Y );
+
+        mCameraController = new CameraController( mGraphicsSystem, false );
+        mGraphicsSystem->getCamera()->setFarClipDistance( 100000.0f );
+        mGraphicsSystem->getCamera()->setPosition( -10.0f, 80.0f, 10.0f );
+
+
+        MeshUtils::importV1Mesh( "tudorhouse.mesh",
+                                 Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME );
+
+        //Create some meshes to show off terrain shadows.
+        Ogre::Item *item = sceneManager->createItem( "tudorhouse.mesh",
+                                                     Ogre::ResourceGroupManager::
+                                                     AUTODETECT_RESOURCE_GROUP_NAME,
+                                                     Ogre::SCENE_STATIC );
+        Ogre::Vector3 objPos( 3.5f, 4.5f, -2.0f );
+        mTerra->getHeightAt( objPos );
+        objPos.y += -std::min( item->getLocalAabb().getMinimum().y, Ogre::Real(0.0f) ) * 0.01f - 0.5f;
+        sceneNode = rootNode->createChildSceneNode( Ogre::SCENE_STATIC, objPos );
+        sceneNode->scale( 0.01f, 0.01f, 0.01f );
+        sceneNode->attachObject( item );
+
+        item = sceneManager->createItem( "tudorhouse.mesh",
+                                         Ogre::ResourceGroupManager::AUTODETECT_RESOURCE_GROUP_NAME,
+                                         Ogre::SCENE_STATIC );
+        objPos = Ogre::Vector3( -3.5f, 4.5f, -2.0f );
+        mTerra->getHeightAt( objPos );
+        objPos.y += -std::min( item->getLocalAabb().getMinimum().y, Ogre::Real(0.0f) ) * 0.01f - 0.5f;
+        sceneNode = rootNode->createChildSceneNode( Ogre::SCENE_STATIC, objPos );
+        sceneNode->scale( 0.01f, 0.01f, 0.01f );
+        sceneNode->attachObject( item );
+
+        TutorialGameState::createScene01();
+    }
+    //-----------------------------------------------------------------------------------
+    void Tutorial_TerrainGameState::destroyScene(void)
+    {
+        Ogre::Root *root = mGraphicsSystem->getRoot();
+        Ogre::Hlms *hlmsPbs = root->getHlmsManager()->getHlms( Ogre::HLMS_PBS );
+
+        //Unset the PBS listener and destroy it
+        if( hlmsPbs->getListener() == mHlmsPbsTerraShadows )
+        {
+            hlmsPbs->setListener( 0 );
+            delete mHlmsPbsTerraShadows;
+            mHlmsPbsTerraShadows = 0;
+        }
+
+        delete mTerra;
+        mTerra = 0;
+
+        TutorialGameState::destroyScene();
+    }
+    //-----------------------------------------------------------------------------------
+    void Tutorial_TerrainGameState::update( float timeSinceLast )
+    {
+        static float accumTime = 0;
+        //mSunLight->setDirection( Ogre::Vector3( cosf( mTimeOfDay ), -sinf( mTimeOfDay ), -1.0 ).normalisedCopy() );
+        //mSunLight->setDirection( Ogre::Vector3( 0, -sinf( mTimeOfDay ), -1.0 ).normalisedCopy() );
+        mSunLight->setDirection( Ogre::Quaternion( Ogre::Radian(mAzimuth), Ogre::Vector3::UNIT_Y ) *
+                                 Ogre::Vector3( cosf( mTimeOfDay ), -sinf( mTimeOfDay ), 0.0 ).normalisedCopy() );
+        //mSunLight->setDirection( -Ogre::Vector3::UNIT_Y );
+
+        //Do not call update() while invisible, as it will cause an assert because the frames
+        //are not advancing, but we're still mapping the same GPU region over and over.
+        if( mGraphicsSystem->getRenderWindow()->isVisible() )
+        {
+            //Force update the shadow map every frame to avoid the feeling we're "cheating" the
+            //user in this sample with higher framerates than what he may encounter in many of
+            //his possible uses.
+            const float lightEpsilon = 0.0f;
+            mTerra->update( mSunLight->getDerivedDirectionUpdated(), lightEpsilon );
+        }
+
+        TutorialGameState::update( timeSinceLast );
+
+        //Camera must be locked to ground *after* we've moved it. Otherwise
+        //fast motion may go below the terrain for 1 or 2 frames.
+        Ogre::Camera *camera = mGraphicsSystem->getCamera();
+        Ogre::Vector3 camPos = camera->getPosition();
+        if( mLockCameraToGround && mTerra->getHeightAt( camPos ) )
+            camera->setPosition( camPos + Ogre::Vector3::UNIT_Y * 10.0f );
+    }
+    //-----------------------------------------------------------------------------------
+    void Tutorial_TerrainGameState::generateDebugText( float timeSinceLast, Ogre::String &outText )
+    {
+        TutorialGameState::generateDebugText( timeSinceLast, outText );
+
+        if( mDisplayHelpMode == 0 )
+        {
+            outText += "\nCtrl+F4 will reload Terra's shaders.";
+        }
+        else if( mDisplayHelpMode == 1 )
+        {
+            char tmp[128];
+            Ogre::LwString str( Ogre::LwString::FromEmptyPointer(tmp, sizeof(tmp)) );
+            Ogre::Vector3 camPos = mGraphicsSystem->getCamera()->getPosition();
+
+            using namespace Ogre;
+
+            outText += "\nF2 Lock Camera to Ground: [";
+            outText += mLockCameraToGround ? "Yes]" : "No]";
+            outText += "\n+/- to change time of day. [";
+            outText += StringConverter::toString( mTimeOfDay * 180.0f / Math::PI ) + "]";
+            outText += "\n9/6 to change azimuth. [";
+            outText += StringConverter::toString( mAzimuth * 180.0f / Math::PI ) + "]";
+            outText += "\n\nCamera: ";
+            str.a( "[", LwString::Float( camPos.x, 2, 2 ), ", ",
+                        LwString::Float( camPos.y, 2, 2 ), ", ",
+                        LwString::Float( camPos.z, 2, 2 ), "]" );
+            outText += str.c_str();
+            outText += "\nLightDir: ";
+            str.clear();
+            str.a( "[", LwString::Float( mSunLight->getDirection().x, 2, 2 ), ", ",
+                        LwString::Float( mSunLight->getDirection().y, 2, 2 ), ", ",
+                        LwString::Float( mSunLight->getDirection().z, 2, 2 ), "]" );
+            outText += str.c_str();
+        }
+    }
+    //-----------------------------------------------------------------------------------
+    void Tutorial_TerrainGameState::keyReleased( const SDL_KeyboardEvent &arg )
+    {
+        if( arg.keysym.sym == SDLK_F4 && (arg.keysym.mod & (KMOD_LCTRL|KMOD_RCTRL)) )
+        {
+            //Hot reload of Terra shaders.
+            Ogre::Root *root = mGraphicsSystem->getRoot();
+            Ogre::HlmsManager *hlmsManager = root->getHlmsManager();
+
+            Ogre::Hlms *hlms = hlmsManager->getHlms( Ogre::HLMS_USER3 );
+            Ogre::GpuProgramManager::getSingleton().clearMicrocodeCache();
+            hlms->reloadFrom( hlms->getDataFolder() );
+        }
+        else if( (arg.keysym.mod & ~(KMOD_NUM|KMOD_CAPS)) != 0 )
+        {
+            TutorialGameState::keyReleased( arg );
+            return;
+        }
+
+        if( arg.keysym.scancode == SDL_SCANCODE_KP_PLUS )
+        {
+            mTimeOfDay += 0.1f;
+            mTimeOfDay = std::min( mTimeOfDay, (float)Ogre::Math::PI );
+        }
+        else if( arg.keysym.scancode == SDL_SCANCODE_MINUS ||
+                 arg.keysym.scancode == SDL_SCANCODE_KP_MINUS )
+        {
+            mTimeOfDay -= 0.1f;
+            mTimeOfDay = std::max( mTimeOfDay, 0.0f );
+        }
+
+        if( arg.keysym.scancode == SDL_SCANCODE_KP_9 )
+        {
+            mAzimuth += 0.1f;
+            mAzimuth = fmodf( mAzimuth, Ogre::Math::TWO_PI );
+        }
+        else if( arg.keysym.scancode == SDL_SCANCODE_KP_6 )
+        {
+            mAzimuth -= 0.1f;
+            mAzimuth = fmodf( mAzimuth, Ogre::Math::TWO_PI );
+            if( mAzimuth < 0 )
+                mAzimuth = Ogre::Math::TWO_PI + mAzimuth;
+        }
+
+        if( arg.keysym.scancode == SDL_SCANCODE_F2 )
+        {
+            mLockCameraToGround = !mLockCameraToGround;
+        }
+        else
+        {
+            TutorialGameState::keyReleased( arg );
+        }
+    }
+}
